8bc46f1897aaf533c69b6ef9f24a5ce0e51c2d16
skuralll <f20154@e.hiroshima-cu.ac.jp>
2023-01-13 13:08:10 +0000

    ステップを進める処理を追加

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 //セル
+#[derive(Debug, Clone)]
 pub struct cell {
     // 座標
     x: i32,
@@ -13,8 +14,20 @@ pub struct Board{
     width: i32,
     height: i32,
     // セルを格納するベクタ
+    
     cells: Vec<cell>,
 }
+
+const AROUND_POS: [(i32, i32); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
 
 impl Board {
     // 盤面を新規生成する
@@ -25,13 +38,18 @@ impl Board {
                 cells.push(cell{x: x, y: y, value: false});
             }
         }
-        Board{cells: cells, width: width, height: height}
+        Self {cells: cells, width: width, height: height}
     }
 
-    //指定の座標のセルを取得する
+    // 指定の座標のセルを取得する
     pub fn get_cell(&self, x: i32, y: i32) -> Option<&cell> {
+        //両端を繋げないための処理
+        if 0 > x && x >= self.width {
+            return None;
+        }
+        //セルが存在していれば返す
         if let Some(cell) = self.cells.get( (x + y*self.width) as usize) {  
-            Some(&cell)
+            Some(cell)
         }
         else{
             None
@@ -39,7 +57,31 @@ impl Board {
     }
 
     // 1ステップ進める
-    pub fn step(){
-
+    pub fn step(&mut self){
+        let mut after_cells: Vec<cell> =self.cells.to_vec(); // 処理終了後のVector
+        for cell in self.cells.iter(){
+            //周りの生存セル数をカウントする
+            let mut around_count = 0;
+            for pos in AROUND_POS.iter(){
+                let x = cell.x + pos.0;
+                let y = cell.y + pos.1;
+                //セル取得
+                if let Some(around_cell) = self.get_cell(x, y){
+                    if around_cell.value{
+                        around_count += 1;
+                    }
+                }
+            }
+            // 生死判定
+            if cell.value && (around_count < 2 || around_count > 3) || 
+              !cell.value && around_count == 3{
+                //生死が変わる場合、aftercell内の該当cellの値を変更する
+               if let Some(after_cell) = after_cells.get_mut((cell.x + cell.y*self.width) as usize){
+                    after_cell.value = !after_cell.value; // 状態反転
+               }
+            }
+        }
+        // cellsをaftercellに置き換える
+        self.cells = after_cells; 
     }
 }
